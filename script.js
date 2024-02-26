06870b4f829f33bfbfb88f915ad3d5e828438484
mohammadaziz05 <61197357+mohammadaziz05@users.noreply.github.com>
2024-02-26 20:03:30 +0530

    Added Ip address function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const displayInfoBtn = document.getElementById('display-info-btn')
 const locationBtn = document.getElementById('location-btn')
+const ipaddressBtn = document.getElementById('ipaddress-btn')
 const displayInfo = document.getElementById('display-info')
 const locationDetails = document.getElementById('location')
+const ipAddress = document.getElementById('ipaddress')
 
 
 
 displayInfoBtn.addEventListener('click', getDisplayInfo)
+locationBtn.addEventListener('click', getLocation) 
+ipaddressBtn.addEventListener('click', getIpaddress) 
 
+
+
+//To display information
 function getDisplayInfo() {
     const display = window.screen
     displayInfo.textContent = `Screen Width : ${display.width}px Screen Height: ${display.height}px`
@@ -14,8 +21,10 @@ function getDisplayInfo() {
 }
 
 
-locationBtn.addEventListener('click', getLocation) 
 
+
+
+//To display location
 function getLocation() {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition, showError)        
@@ -31,7 +40,8 @@ function getLocation() {
 function showPosition (position) {
         const latitude = position.coords.latitude
         const longitude = position.coords.longitude
-        locationDetails.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`
+        locationDetails.textContent = `Latitude: ${latitude}
+                                       Longitude: ${longitude}`
 }
 
 function showError (error) {
@@ -44,4 +54,15 @@ function showError (error) {
     else if(error.code === error.UNKNOWN_ERROR) {
         locationDetails.textContent = `An unknown error occurred.`
     }
+}
+
+
+
+//To Display Ip address
+
+function getIpaddress(){
+    $.getJSON('https://ipinfo.io/json', function(data){
+        ipAddress.textContent = `${data.ip}`
+        ipAddress.classList.add('show')
+    })
 }
